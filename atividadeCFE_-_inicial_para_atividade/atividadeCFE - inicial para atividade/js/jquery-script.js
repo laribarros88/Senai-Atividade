b38442ea6ebcfc5bc5b1cc34891c37038f34fcfe
laribarros88 <laribarros88@gmail.com>
2025-11-27 23:10:32 -0300

    UC 09 - ATIVIDADE 02

---
+++ atividadeCFE_-_inicial_para_atividade/atividadeCFE - inicial para atividade/js/jquery-script.js
@@ -0,0 +1,15 @@
+//efeito de esconder formulário de cadastro
+
+$(document).ready(function(){
+    
+    $$("#botao-cadastrar").click(function(){
+
+        $("#form-cadastrar").slideToggle("slow");
+        $("#form-login").slideToggle("slow");
+        $("#botao-cadastrar").hide();
+
+    });        
+
+});
+
+
